74049f6ec2c61bacd08c35e95cf54278d2063874
somaye <s.smousvy@yahoo.com>
2024-11-27 12:04:08 +0330

    build folder added

---
+++ build/precache-manifest.6fbbeb3b6da14a1c3559ba0b3646bd57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5da378ebb8616fbec0edaa2a83d10d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a992503ad0db4d918ba3",
+    "url": "/static/css/main.5546f20a.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "a992503ad0db4d918ba3",
+    "url": "/static/js/main.3b26c752.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
